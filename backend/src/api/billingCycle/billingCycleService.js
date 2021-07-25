2a61d30d729582255e7d166b21a142916acf1875
rodolforomera <rodolforomeras@gmail.com>
2021-07-25 14:43:49 -0300

    Mapeamento das rotas padrões de BillingCycle e rota count

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,26 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidator: true})
+
+BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if(!err) {
+            res.json(docs)
+        }else{
+            res.status(500).json({errors: [error]})
+        }
+    })
+})
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
+module.exports = BillingCycle
