ba041b6b7a32d8372adfee81105b8742a760e55a
rodolforomera <rodolforomeras@gmail.com>
2021-07-31 15:07:46 -0300

    Criado componente Dashboard usando tanto  o redux quanto sem (Dashboard2)

--- frontend/src/dashboard2/dashboard2.jsx
+++ frontend/src/dashboard2/dashboard2.jsx
@@ -1,26 +1,32 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreator, bindActionCreators } from 'redux'
+import axios from 'axios'
 
-import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
-class Dashboard extends Component {
+const BASE_URL = 'http://localhost:3003/api'
+
+export default class Dashboard extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {credit: 0, debt: 0}
+    }
 
     componentDidMount() {
-        this.props.getSummary()
+        axios.get(`${BASE_URL}/billingCycles/summary`)
+            .then(resp => this.setState(resp.data))
     }
     
     render() {
 
-        const { credit, debt } = this.props.summary
+        const { credit, debt } = this.state
 
         return (
             <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <ContentHeader title='Dashboard' small='Versão 2.0 (Sem Redux)' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
@@ -35,10 +41,3 @@ class Dashboard extends Component {
         )
     }
 }
-
-const mapStateToProps = 
-    state => ({summary: state.dashboard.summary})
-const mapDispatchToProps = 
-    dispatch => bindActionCreators({getSummary}, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
