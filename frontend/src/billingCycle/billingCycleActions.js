456452f24f28d4833d4732e43589094bc7142a5b
rodolforomera <rodolforomeras@gmail.com>
2021-08-14 09:26:07 -0300

    Criado BillingCycleForm (Inclusão e mensagens de sucesso e erro)

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -6,5 +10,25 @@ export function getList() {
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
+    }
+}
+
+export function create(values) {
+    //redux-thunk
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso!')
+                //redux-multi (permite passar um array de actions)
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error));
+            })
     }
 }
