e88dd93e9beacf6e7dca43148d9545087c51dfc5
rodolforomera <rodolforomeras@gmail.com>
2021-08-22 15:05:07 -0300

    Atualizadas versões de algumas dependencias tanto no front quando no backend. Criado o front pelo create-react-app. A versão com o webpack agora está como frontend-old. Utilizando a versão 17 do react. Alterada forma do Routes devido atualização da dependencia

---
+++ frontend-old/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021
+            <a href='http://mymoneyapp.com.br' target='_blank'> MyMoneyApp</a>.
+        </strong>
+    </footer>
+)
